d16386ac7f5e272ba1760420c5c497414c6125c0
Harshad Vadakkepura <harshadvad@gmail.com>
2026-07-21 09:40:39 +1000

    Scaffold /promo landing page route

---
+++ components/pages/promo.tsx
@@ -0,0 +1,22 @@
+import { Nav } from '@/components/shared/nav';
+import { Footer } from '@/components/shared/footer';
+
+export function Promo() {
+  return (
+    <div className="pd-page">
+      <Nav active="promo" />
+
+      {/* HERO_SECTION */}
+
+      {/* PHOTO_ROW_SECTION */}
+
+      {/* INCLUDED_SECTION */}
+
+      {/* FORM_SECTION */}
+
+      {/* FINE_PRINT_SECTION */}
+
+      <Footer />
+    </div>
+  );
+}
